bcdf5c95b7aefe29469268d83049fa6732c02f6e
Bolous AbuJaber <ajbolous@gmail.com>
2016-04-26 11:16:05 -0700

    classes

--- prototype/GHealth/src/Model/ExaminationReferral.java
+++ prototype/GHealth/src/Model/ExaminationReferral.java
@@ -6,38 +6,9 @@ public class ExaminationReferral  {
 	private int id;
 	private Patient patient;
 	private Date time;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public Patient getPatient() {
-		return patient;
-	}
-	public void setPatient(Patient patient) {
-		this.patient = patient;
-	}
-	public Date getTime() {
-		return time;
-	}
-	public void setTime(Date time) {
-		this.time = time;
-	}
-	public String getComments() {
-		return comments;
-	}
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-	public Labratory getLabratory() {
-		return labratory;
-	}
-	public void setLabratory(Labratory labratory) {
-		this.labratory = labratory;
-	}
 	private String comments;
 	private Labratory labratory;
+	
 	public int getId() {
 		return id;
 	}

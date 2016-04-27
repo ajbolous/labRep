93b308afb50daf7bf4d579e9fcf5c19503a22a07
Ahdab Serhan <ms.ahdab@gmail.com>
2016-04-27 19:50:38 +0300

    Revert "update"
    This reverts commit c97abb73fe436bbf6bd54a12b2886d737f492101.

--- prototype/GClient/src/Controllers/UsersController.java
+++ prototype/GClient/src/Controllers/UsersController.java
@@ -1,38 +1,5 @@
 package Controllers;
-import ClientUI.Application;
-import Users.User;
-import Utils.Request;
-public class UsersController {
-	
-	public User getUser(String id) throws Exception
-	{
-		Request r = new Request("users/byId");
-		r.addParam("id", id);
-		
-		User u = (User)Application.client.Request(r);
-		
-		if(u == null){
-			
-			throw new Exception();
-		}
-		return u;
-	
-	
-	}
-	
-	public boolean authenticateUser(String id, String pass){
-		User u = getUser(id);
-		
-		if(u == null)
-			return false;
 
-		if(u.getPassword().equals(pass))
-			return true;
-		
-		return false;
-		
-	}
+public class UsersController {
 
 }
-
-
